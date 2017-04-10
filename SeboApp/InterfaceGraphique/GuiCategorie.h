b7d6637e2f7e8eaf4fae5c393a2c49e26bc29edc
Corentin Rogeré <corentin.rogere@gmail.com>
2017-04-10 13:48:03 +0200

    essai

--- SeboApp/InterfaceGraphique/GuiCategorie.h
+++ SeboApp/InterfaceGraphique/GuiCategorie.h
@@ -1,14 +1,7 @@
 #pragma once
 
 #include <QWidget>
-#include "ui_GuiCategorie.h"
-#include "Tools/Connexion.h"
-#include <qsqldatabase.h>
-#include <qsqltablemodel.h>
-#include "DAO\ManagerCategorie.h"
-#include "Entity\Categorie.h"
-#include <vector>
-#include <qmessagebox.h>
+namespace Ui { class GuiCategorie; };
 
 class GuiCategorie : public QWidget
 {
@@ -17,11 +10,6 @@ class GuiCategorie : public QWidget
 public:
 	GuiCategorie(QWidget *parent = Q_NULLPTR);
 	~GuiCategorie();
-	
-
-public slots:
-	void ajouterArticle();
-	void majTable();
 
 private:
 	Ui::GuiCategorie *ui;
